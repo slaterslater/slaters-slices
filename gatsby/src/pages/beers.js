58652077880b36290dc129024857637ac7b4f888
slaterslater <slaterslater@gmail.com>
2021-03-13 14:17:18 -0500

    add page scaffolding

---
+++ gatsby/src/pages/beers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BeerPage() {
+  return (
+    <div>
+      <p>beer</p>
+    </div>
+  );
+}
---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <p>hey bruv it's me</p>
+    </div>
+  );
+}
