16a717e8deeacf2b34e3ff73d699fe7961c47a51
slaterslater <slaterslater@gmail.com>
2021-03-21 18:47:51 -0400

    add pizzas query and SinglePizza component

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzaPage() {
+export default function PizzaPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+  // console.log(data);
   return (
     <>
-      <p>pizzas</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
