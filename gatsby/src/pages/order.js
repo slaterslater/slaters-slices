6f90e091aaf48ff7cacf1f87b37ad54353c58797
slaterslater <slaterslater@gmail.com>
2021-03-29 22:51:36 -0400

    update orders.js * add useForm and usePizza hooks * add MenuItemStyles and OrderStyles * add PizzaOrder

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,9 +1,97 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
+import calculatePizzaPrice from '../utils/calculatePizzaPrice';
+import formatMoney from '../utils/formatMoney';
+import OrderStyles from '../styles/OrderStyles';
+import MenuItemStyles from '../styles/MenuItemStyles';
+import usePizza from '../utils/usePizza';
+import PizzaOrder from '../components/PizzaOrder';
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
+  const { order, addToOrder, removeFromOrder } = usePizza({
+    pizzas,
+    inputs: values,
+  });
   return (
     <>
-      <p>order</p>
+      <SEO title="Place your order" />
+      <OrderStyles>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValue}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
+        </fieldset>
+        <fieldset className="menu">
+          <legend>Menu</legend>
+          {pizzas.map((pizza) => (
+            <MenuItemStyles key={pizza.id}>
+              <Img width="50" height="50" fluid={pizza.image.asset.fluid} />
+              <div>
+                <h2>{pizza.name}</h2>
+              </div>
+              <div>
+                {['S', 'M', 'L'].map((size) => (
+                  <button
+                    key={`${pizza.name}-${size}`}
+                    type="button"
+                    onClick={() => addToOrder({ id: pizza.id, size })}
+                  >
+                    {size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
+                  </button>
+                ))}
+              </div>
+            </MenuItemStyles>
+          ))}
+        </fieldset>
+        <fieldset className="order">
+          <legend>Order Summary</legend>
+          <PizzaOrder
+            order={order}
+            removeFromOrder={removeFromOrder}
+            pizzas={pizzas}
+          />
+        </fieldset>
+      </OrderStyles>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        price
+        id
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
