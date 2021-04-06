88833e756ab5ab3f7f190549065c4bd76f0912fa
slaterslater <slaterslater@gmail.com>
2021-04-06 19:28:36 -0400

    add serverless function to handle pizza order email

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,35 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  const today = new Date();
+  const date = today.toDateString();
+  const time = `${today.getHours()}:${today.getMinutes()}`;
+
+  return `<div>
+    <h2>Your Recent Pizza Order from Slater's Slices</h2>
+    <p><strong>Time placed</strong>: ${date} at ${time}</p>
+    <p><strong>Total</strong>: ${total}</p>
+    <ul>
+      ${order
+        .map(
+          (item) => `
+        <li>
+          ${item.size} - ${item.name} - ${item.price}
+          <br>
+          <img src=${item.thumbnail} atl=${item.name} />
+        </li>
+      `
+        )
+        .join('')}
+    </ul>
+    <p><strong>Total</strong>: ${total}</p>
+    <style>
+        ul {list-style:none}
+        li {margin-bottom:5px}
+    </style>
+  </div>`;
+}
+
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: 587,
@@ -10,13 +40,33 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+  // validate data
+  const requiredFields = ['email', 'name', 'order'];
+
+  for (const field of requiredFields) {
+    console.log(`checking if the ${field} is good`);
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Please take a look at the ${field} field?`,
+        }),
+      };
+    }
+  }
+  // send the email
+
+  // send success or error
+
   const info = await transporter.sendMail({
     from: "Slater's Slices <slater@example.com>",
     to: 'orders@example.com',
-    subject: 'New Order!',
-    html: `<p>Your Pizza Order</p>`,
+    subject: 'Your Pizza Order',
+    html: generateOrderEmail(body),
   });
-  console.log(info);
+  // console.log(info);
   return {
     statusCode: 200,
     body: JSON.stringify(info),
--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,10 +1,16 @@
 import { useContext, useState } from 'react';
 import OrderContext from '../components/OrderContext';
+import attachNamesAndPrices from './attachNamesAndPrices';
+import calculateOrderTotal from './calculateOrderTotal';
+import formatMoney from './formatMoney';
 
-export default function usePizza({ pizzas, inputs }) {
+export default function usePizza({ pizzas, values }) {
   // const [order, setOrder] = useState([]);
   // moving useState up to the provider
   const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState();
   function addToOrder(orderedPizza) {
     setOrder([...order, orderedPizza]);
   }
@@ -12,11 +18,44 @@ export default function usePizza({ pizzas, inputs }) {
   function removeFromOrder(index) {
     setOrder([...order.slice(0, index), ...order.slice(index + 1)]);
   }
-  // TODO send this data to serverless function
-  //
+
+  async function submitOrder(e) {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    setMessage(null);
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+    };
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const text = JSON.parse(await res.text());
+    setLoading(false);
+    if (res.status >= 400) {
+      setError(text.message);
+    } else {
+      setMessage('Your order was placed successfully!');
+    }
+  }
+
   return {
     order,
     addToOrder,
     removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
   };
 }
